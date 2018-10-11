cfcb204912144cce602b41cdc15d8f6797e82f8c
Ricardo Nava <ricardo.a.nava@gmail.com>
2018-10-10 21:18:36 -0700

    refactored code with the help of airbnb eslint config

--- main.js
+++ main.js
@@ -1,31 +1,10 @@
 const soap = require('soap');
-const parseString = require('xml2js').parseString;
+const { parseString } = require('xml2js');
 const express = require('express');
 
 const url = 'http://www.banxico.org.mx/DgieWSWeb/DgieWS?WSDL';
 
-const currencySource = url => {
-  soap.createClient(url, (err, WSDLClient) => {
-    clientValues(WSDLClient);
-  });
-};
-
-const clientValues = WSDLClient => {
-  WSDLClient.DgieWS.DgieWSPort.tiposDeCambioBanxico(
-    (args = null),
-    (err, result) => {
-      parseXML(result.result.$value);
-    }
-  );
-};
-
-const parseXML = xml => {
-  parseString(xml, (err, parseResult) => {
-    expressRoutes(parseResult);
-  });
-};
-
-const expressRoutes = parseResult => {
+const expressRoutes = (parseResult) => {
   const exchangeArray = parseResult.CompactData['bm:DataSet'][0]['bm:Series'];
 
   const currencies = {};
@@ -35,18 +14,15 @@ const expressRoutes = parseResult => {
       valor: exchangeArray[index]['bm:Obs'][0].$.OBS_VALUE,
       descripcion: exchangeArray[index].$.TITULO,
       periodo: exchangeArray[index]['bm:Obs'][0].$.TIME_PERIOD,
-      serie: exchangeArray[index].$.IDSERIE
+      serie: exchangeArray[index].$.IDSERIE,
     };
   });
 
   const app = express();
 
-  app.use(function(req, res, next) {
+  app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
@@ -54,6 +30,24 @@ const expressRoutes = parseResult => {
 
   app.listen(3005, () => console.log('Example app listening on port 3005!'));
   // app.listen(process.env.PORT);
+};
+
+const parseXML = (xml) => {
+  parseString(xml, (err, parseResult) => {
+    expressRoutes(parseResult);
+  });
+};
+
+const clientValues = (WSDLClient) => {
+  WSDLClient.DgieWS.DgieWSPort.tiposDeCambioBanxico(null, (err, result) => {
+    parseXML(result.result.$value);
+  });
+};
+
+const currencySource = () => {
+  soap.createClient(url, (err, WSDLClient) => {
+    clientValues(WSDLClient);
+  });
 };
 
 currencySource(url);
